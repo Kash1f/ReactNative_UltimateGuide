ebc6aa19adfdebfb9afd7fc721669db3da9b5963
Kashif <ahmed.kashif1001@gmail.com>
2024-09-27 23:53:28 +0500

    get api method

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import GET_API from './src/components/GET_API'
+
+const App = () => {
+  return (
+    <View>
+      <GET_API/>
+    </View>
+  )
+}
+
+export default App
---
+++ src/components/GET_API.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GET_API = () => {
+  return (
+    <View>
+      <Text>GET API Method</Text>
+    </View>
+  )
+}
+
+export default GET_API
