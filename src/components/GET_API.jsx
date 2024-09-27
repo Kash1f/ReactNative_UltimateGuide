ae127cc9953d5042314f053bf6141739d73cbf11
Kashif <ahmed.kashif1001@gmail.com>
2024-09-28 00:38:49 +0500

    lifecycle components, useEffect

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const GET_API = () => {
+
+    //component mount hone se pehle liefecycle method call hota hai, and saray lc method useEffect k andar hote hain, 
+    //apni api ko pehle hi load karwa dena hai UI render hone se pehle, taa k data ko UI pe render karwa rahe ho to
+    //UI render hone se pehle data API se fetch ho k achuka ho
+
+    useEffect (()=>{
+
+    },[]);
+
+
   return (
     <View>
-      <Text>GET API Method</Text>
+      <Text style={{fontSize: 24, marginBottom:1-0}}>GET API Method</Text>
     </View>
   )
 }
