d885252a50e6fbece02d1633abf474817a10febb
Kashif <ahmed.kashif1001@gmail.com>
2024-09-29 23:28:53 +0500

    async storage example

--- App.jsx
+++ App.jsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import GET_API from './src/components/GET_API'
 import AnimatedCardFlip from './src/components/AnimatedCardFlip'
+import AsyncStorageExample from './src/components/AsyncStorageExample'
 
 const App = () => {
   return (
     <View>
       <GET_API/>
       {/* <AnimatedCardFlip/> */}
+      {/* <AsyncStorageExample/> */}
     </View>
   )
 }
