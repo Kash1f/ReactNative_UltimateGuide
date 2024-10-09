fd98142124aa342956c2ab697d3c61fb3b1047d3
Kashif <ahmed.kashif1001@gmail.com>
2024-10-09 23:15:30 +0500

    array iteration, map method to display array of objects

--- src/components/GET_API.jsx
+++ src/components/GET_API.jsx
@@ -1,35 +1,39 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const GET_API = () => {
+  const [myData, setMyData] = useState([]);
 
-    //component mount hone se pehle liefecycle method call hota hai, and saray lc method useEffect k andar hote hain, 
-    //apni api ko pehle hi load karwa dena hai UI render hone se pehle, taa k data ko UI pe render karwa rahe ho to
-    //UI render hone se pehle data API se fetch ho k achuka ho
+  //component mount hone se pehle liefecycle method call hota hai, and saray lc method useEffect k andar hote hain,
+  //apni api ko pehle hi load karwa dena hai UI render hone se pehle, taa k data ko UI pe render karwa rahe ho to
+  //UI render hone se pehle data API se fetch ho k achuka ho
 
-    useEffect (()=>{
-       
-
-      axios.get("https://api.example.com/data");
-      .then(response => console.log(data));
-      .catch(error => console.log(error));
-      )
-
-    },[]);
-
+  useEffect(() => {
+    axios.get('http://10.0.2.2:3000/users').then(result => {
+      // console.log(result.data);
+      setMyData(result.data);  //myData me data ko store karwaiga jab result.data aaiga, array wale data ko myData me update karwa dega setMyData
+    })
+    .catch(error => {
+      console.log(error);
+      
+    })
+  }, []);
 
   return (
     <View>
-      <Text style={{fontSize: 24, marginBottom:1-0}}>GET API Method</Text>
+      <Text style={{fontSize: 24, marginBottom:1}}>GET API Method</Text>
+      {
+        //map will let us iterate over the array of objects
+        myData.map((item)=>{
+          <Text>{item.name}</Text>
+        })
+      }
     </View>
-  )
-}
+  );
+};
 
 export default GET_API;
-
-
-
 
 //old code:
 
