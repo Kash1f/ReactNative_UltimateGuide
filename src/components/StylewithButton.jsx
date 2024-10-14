f7da5845f810534f46261e4d9739a1deead1efd9
Kashif <ahmed.kashif1001@gmail.com>
2024-10-14 23:58:24 +0500

    style with button, touchable opacity

--- src/components/StylewithButton.jsx
+++ src/components/StylewithButton.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const StylewithButton = () => {
   return (
     <View>
-      <Text>StylewithButton</Text>
-    </View>
-  )
+    <Button title="Basic Button" color={'red'}></Button>
+    <TouchableOpacity style={StyleSheet.button}>
+        <Text style={StyleSheet.buttonText}></Text>
+        </TouchableOpacity>
+</View>
+)
 }
 
 export default StylewithButton
+
+const styles = StyleSheet.create({
+    button : {
+        margin: 20,
+        padding:20
+    }
+})
