6d9be62864f1c198b80b0f115d2ea6a868902106
Kashif <ahmed.kashif1001@gmail.com>
2024-10-31 16:36:54 +0500

    bug fixes

---
+++ src/components/MyLoginNativeWind.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyLoginNativeWind = () => {
+  return (
+    <View>
+      <Text>MyLoginNativeWind</Text>
+    </View>
+  )
+}
+
+export default MyLoginNativeWind
--- src/components/StatusBarExample.jsx
+++ src/components/StatusBarExample.jsx
@@ -1,42 +1,36 @@
-import {View, Text, StatusBar, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import My from './My';
 
-const StatusBarExample = () => {
+const HideShowToggle = () => {
+  const [status, setStatus] = useState(true);
+
   return (
-    <View style={styles.container}>
-      <StatusBar
-        barStyle={'light-content'}
-        backgroundColor="#6200Fe"
-        hidden={false}
-     
-      />
-      <Text style={styles.text}>StatusBar</Text>
+    <View>
+      <Text style={{fontSize: 30}}>HideShowToggle</Text>
+      {
+        // ? agar status ki value true hai to My comp show hojaiga, : agar false hai to null show hojaiga, My ki value hide hojaigi
+        status ? <My /> : null
+      }
+      {/* <Button title="Hide" onPress={() => setStatus(false)} />
+      <Button title="Show" onPress={() => setStatus(true)} /> */}
+      <Button title="Toggle" onPress={() => setStatus(!status)} />
     </View>
   );
 };
 
-export default StatusBarExample;
+export default HideShowToggle;
 
-const styles = StyleSheet.create({
-  container: {
-    flex : 1,
-    justifyContent: 'center',
-    alignItems : 'center',
-    backgroundColor : '#6200fe'
-  },
-    text : {
-      color: '#FFF'
-    }
-})
 /*
 
-How to Style Status BarExplanation:
+Explanation of this code:
 
-Let's say we have an application with a red theme and we want the entire screen to be red, or we want the StatusBar to be hidden if playing a game. So, we can style the status bar in React Native in many ways.
+1. Jab button pe press kia jaiga to setStatus ki value false hojaigi yaani current state i.e status will be false, pehle true tha means component show ho raha tha but value false hone k baad hide hogya
 
-StatusBar component is self closed and works with some properties.
+2. Show karne k liye setStatus ki value true rakhni hai taa k jab hide ho to status value false hogi but Show pe setStatus function ki value true hai to status ki value true hojaigi
 
-hidden = {true} - To hide content in games etc. By default value of StatusBar is {false}
-barStyle = default, dark-content, light-content - According to your preference
+Now to toggle hide and show with one button:
+
+1. By default status ki value true hai but now Button k click pe setStatus ki value !status karni hai kyun k jo bhi state hogi ye uska opposite kardega means agar true hai to component show hoga but jab button press hoga to true false hojaiga and comp hide hojaiga and similarly jab dubara press hoga button to false se true hojaiga means component show hojaiga
 
 */
