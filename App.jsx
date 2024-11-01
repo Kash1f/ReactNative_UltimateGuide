46525b2f607736f9678fba4160553899c35bddad
Kashif <ahmed.kashif1001@gmail.com>
2024-11-01 23:29:18 +0500

    tailwind css setup for react native cli

--- App.jsx
+++ App.jsx
@@ -1,39 +1,53 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
-import GET_API from './src/components/GET_API'
-import AnimatedCardFlip from './src/components/AnimatedCardFlip'
-import AsyncStorageExample from './src/components/AsyncStorageExample'
-import POST_API from './src/components/POST_API'
-import LoginForm from './src/components/LoginForm'
-import DELETE_API from './src/components/DELETE_API'
-import FlatListScreen from './src/components/FlatListScreen'
-import HideShowToggle from './src/components/HideShowToggle'
-import StatusBarExample from './src/components/StatusBarExample'
+import {SafeAreaView} from 'react-native';
+import React from 'react';
+import GET_API from './src/components/GET_API';
+import AnimatedCardFlip from './src/components/AnimatedCardFlip';
+import AsyncStorageExample from './src/components/AsyncStorageExample';
+import POST_API from './src/components/POST_API';
+import LoginForm from './src/components/LoginForm';
+import DELETE_API from './src/components/DELETE_API';
+import FlatListScreen from './src/components/FlatListScreen';
+import HideShowToggle from './src/components/HideShowToggle';
+import StatusBarExample from './src/components/StatusBarExample';
+import MyLogin from './src/components/MyLogin';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <LoginForm/> */}
-      {/* <FlatListScreen/> */}
-      {/* <HideShowToggle/> */}
-      <StatusBarExample/>
-      {/* <GET_API/> */}
-      {/* <POST_API/> */}
-      {/* <DELETE_API/> */}
-      {/* <AnimatedCardFlip/> */}
-      {/* <AsyncStorageExample/> */}
-      
-      
-     
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1">
+      <MyLogin />
+    </SafeAreaView>
+  );
+};
 
 export default App;
 
+//
+{
+  /* <LoginForm/> */
+}
+{
+  /* <FlatListScreen/> */
+}
+{
+  /* <HideShowToggle/> */
+}
+{
+  /* <StatusBarExample/> */
+}
+{
+  /* <GET_API/> */
+}
+{
+  /* <POST_API/> */
+}
+{
+  /* <DELETE_API/> */
+}
+{
+  /* <AnimatedCardFlip/> */
+}
+{
+  /* <AsyncStorageExample/> */
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex : 1,
-  },
-})
+//
---
+++ src/components/MyLogin.jsx
@@ -0,0 +1,20 @@
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+
+const MyLoginNativeWind = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <View className='flex-1 justify-center items-center bg-gray-900'>
+      <Text className='text-4xl font-bold text-white mb-8'>Welcome Back!</Text>
+      <TextInput
+      placeholder='Enter your email'
+      placeholderTextColor={'#ccc'}
+      className='w-full h-12 bg-gray-800 rounded-lg text-white px-4 mb-4'
+      value=''
+      />
+    </View>
+  )
+};    
+
+export default MyLoginNativeWind
