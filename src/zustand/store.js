7cd7ebec4bfa66ebebf0e0c9884b9c53d04fdbc9
Kashif <ahmed.kashif1001@gmail.com>
2024-11-02 23:10:22 +0500

    zustand

---
+++ src/zustand/store.js
@@ -0,0 +1,8 @@
+import {create} from 'zustand';
+
+export const useStore = create((set) => ({
+  count: 1,
+  increment: () => set((state) => ({count: state.count + 1})),
+  decrement: () => set((state) => ({count: state.count - 1})),
+  reset: () => set({count: 1}),
+}));
